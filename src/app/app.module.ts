b2a7246fe6d99e5509f7e732f4c99c0b36faa346
Laura Camacho <lauracamacholeon@gmail.com>
2021-08-29 15:23:17 -0500

    modularizado terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-import { TasksComponent } from './pages/tasks/tasks.component';
-
-
 // mis modulos
-import { ComponentsModule } from './components/components.module';
-import { PipesModule } from './pipes/pipes.module';
 import { SharedModule } from './shared/shared.module';
-
-// modulos
+import { PagesModule } from './pages/pages.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent,  
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
     // mis modulos 
-    ComponentsModule,
-    PipesModule,
-    SharedModule
+    SharedModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksComponent } from './tasks/tasks.component';
-
-
+import { ComponentsModule } from '../components/components.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
-  declarations: [
-    TasksComponent
-  ],
+  declarations: [TasksComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    // mis modulos
+    ComponentsModule,
+    PipesModule,
   ],
-  exports:[
-    TasksComponent
-  ]
+  exports: [TasksComponent],
 })
-export class PagesModule { }
+export class PagesModule {}
